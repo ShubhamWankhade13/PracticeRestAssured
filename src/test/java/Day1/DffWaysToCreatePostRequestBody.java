9fecb6f4f242f8f276ffa3e3a2633691e09281e2
Shubham Wankhade <shubhamwankhade@Shubhams-MacBook-Air.local>
2025-10-25 10:36:20 +0530

    this is my restAssured repo

---
+++ src/test/java/Day1/DffWaysToCreatePostRequestBody.java
@@ -0,0 +1,113 @@
+package Day1;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+
+public class DffWaysToCreatePostRequestBody {
+	
+	
+	//Post request using HashMap
+	//@Test(priority=1)
+	void testPostUsingHashMap() {
+		
+		HashMap data = new HashMap();
+		data.put("name", "shubham");
+		data.put("job", "Engineer");
+		data.put("phone", "12345");
+		
+		String coursesArr[] = {"c","c++"};
+		data.put("courses", coursesArr);
+		
+		
+		given()
+		.contentType("application/json")
+		.body(data)
+		.header("x-api-key", "reqres-free-v1")
+		
+		.when()
+		.post("https://reqres.in/api/users")
+		
+		.then()
+		.statusCode(201)
+		.body("name", equalTo("shubham"))
+		.body("job", equalTo("Engineer"))
+		.body("phone", equalTo("12345"))
+		.body("courses[0]", equalTo("c"))
+		.body("courses[1]", equalTo("c++"))
+		.header("Content-Type", "application/json; charset=utf-8")
+		.log().all();
+		
+	}
+	//@Test
+	void testPostUsingJsonLibrary() {
+		
+		JSONObject data = new JSONObject();
+		
+		data.put("name", "shubham");
+		data.put("job", "Engineer");
+		data.put("phone", "12345");
+		
+		String coursesArr[] = {"c","c++"};
+		data.put("courses", coursesArr);
+		
+		
+		given()
+		.contentType("application/json")
+		.body(data.toString())
+		.header("x-api-key", "reqres-free-v1")
+		
+		.when()
+		.post("https://reqres.in/api/users")
+		
+		.then()
+		.statusCode(201)
+		.body("name", equalTo("shubham"))
+		.body("job", equalTo("Engineer"))
+		.body("phone", equalTo("12345"))
+		.body("courses[0]", equalTo("c"))
+		.body("courses[1]", equalTo("c++"))
+		.header("Content-Type", "application/json; charset=utf-8")
+		.log().all();
+	}
+	
+	@Test
+	void testPostUsingPojoClass() {
+		
+		PojoClass data = new PojoClass();
+		data.setName("shubham");
+		data.setJob("Engineer");
+		data.setPhone("12345");
+		
+		String courseArr[]= {"c", "c++"};
+		data.setCourses(courseArr);
+		
+		
+		given()
+		.contentType("application/json")
+		.body(data)
+		.header("x-api-key", "reqres-free-v1")
+		
+		.when()
+		.post("https://reqres.in/api/users")
+		
+		.then()
+		.statusCode(201)
+		.body("name", equalTo("shubham"))
+		.body("job", equalTo("Engineer"))
+		.body("phone", equalTo("12345"))
+		.body("courses[0]", equalTo("c"))
+		.body("courses[1]", equalTo("c++"))
+		.header("Content-Type", "application/json; charset=utf-8")
+		.log().all();
+	
+		
+	}
+	
+
+}
---
+++ src/test/java/Day1/PojoClass.java
@@ -0,0 +1,38 @@
+package Day1;
+
+public class PojoClass {
+
+	String name;
+	String job;
+	String phone;
+	String[] Courses;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String[] getCourses() {
+		return Courses;
+	}
+	public void setCourses(String[] courses) {
+		this.Courses = courses;
+	}
+	
+	
+	
+}
